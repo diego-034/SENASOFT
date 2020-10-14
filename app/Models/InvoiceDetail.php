b9bc7803156e7ba0d8d4839bcaf40c0beda1b123
Diego <dacantero47@misena.edu.co>
2020-10-14 11:24:04 -0500

    Create Models

--- app/Models/InvoiceDetail.php
+++ app/Models/InvoiceDetail.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserType extends Model
+class InvoiceDetail extends Model
 {
     //
 }
--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SearchPreference extends Model
+class Notification extends Model
 {
     //
 }
