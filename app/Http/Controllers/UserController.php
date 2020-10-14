a7035d02f77802fa47188cd3a2344f072b256815
ospinajuan2000 <ospinajuan2000@gmail.com>
2020-10-14 17:37:22 -0500

    Métodos y modelo de user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\User;
 use App\Models\UserType;
 use Illuminate\Http\Request;
+use App\Repositories\IRepository\IModelRepository;
+use Exception;
+use App\Json\Json;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+
+    private $IModelRepository;
+    private $User;
+
+    public function __construct(IModelRepository $IModelRepository)
+    {
+        $this->IModelRepository = $IModelRepository;
+        $this->User = new User();
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function List()
+    public function List(Request $request)
     {
-        return view('users/users');
+        try {
+            if($request->isMethod('GET')) {
+                return view('users.users');
+            }
+            $data = [];
+            $data['Model'] = $this->User;
+            $data['Query'] = [
+                'id',
+                'name',
+                'lastname',
+                'address',
+                'phone',
+                'created_at'
+            ];
+            $data['Row'] = 'name';
+            $response = Json::Json($request, $data);
+            return response()->json($response);
+        } catch (Exception $ex) {
+            return $this->SendError([$ex->getMessage()]);
+        }
     }
 
     /**
@@ -23,12 +56,17 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function Insert()
+    public function Insert(Request $request)
     {
+        try {
+            if ($request->isMethod('GET')) {
+                $Roles = UserType::all()->toArray();
+                return view('users.form-create',['Roles' => $Roles]);
+            }
 
-        $Roles = UserType::all()->toArray();
-
-        return view('users/form-create',['Roles' => $Roles]);
+        } catch (Exception $ex) {
+            return view('error');
+        }
     }
 
     /**
@@ -39,7 +77,7 @@ class UserController extends Controller
      */
     public function Update(UserType $userType)
     {
-        return view('users/form-update');
+        return view('users.form-update');
     }
 
     /**
@@ -48,8 +86,41 @@ class UserController extends Controller
      * @param  \App\UserType  $userType
      * @return \Illuminate\Http\Response
      */
-    public function Delete(UserType $userType)
+
+    public function Delete($id)
     {
-        
+        try {
+            $data = [];
+            $data['Model'] = $this->User;
+            $data['Entity']['id'] = $id;
+            $response = $this->IModelRepository->Delete($data);
+            if (isset($response['Error'])) {
+                return response()->json(false);
+            }
+            return response()->json($response['OK']);
+        } catch (Exception $ex) {
+            return response()->json(false);
+        }
+    }
+
+    /**
+     * Search the specified resource with the filter.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function Find()
+    {
+        try {
+            $data = [];
+            $data['Model'] = $this->User;
+            $response = $this->IModelRepository->Delete($data);
+            if (isset($response['Error'])) {
+                throw new Exception($response['Error']->getMessage());
+            }
+            return view('users.users');
+        } catch (Exception $ex) {
+            return view('error');
+        }
     }
 }
