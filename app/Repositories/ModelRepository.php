3ab9f90ac06dd3db2f63ccba630ae2eb12b2afac
Diego <dacantero47@misena.edu.co>
2020-10-14 11:58:57 -0500

    Changes in Repository

--- app/Repositories/ModelRepository.php
+++ app/Repositories/ModelRepository.php
@@ -9,19 +9,30 @@ class ModelRepository implements IModelRepository {
 
     private $Model;
 
+    /**
+     * Set the model private
+     *
+     * @param Model Model send controller to repository
+     */
     public function SetModel($Model)
     {
         $this->Model = $Model;
     }
 
+    /**
+     * List all data of DB
+     *
+     * @param Array Model, Data for search in group for words
+     * @return Array Data of DB in $response
+     */
     public function List($data) 
     {
+        $response = array();
         try {
-            $response = [];
             $this->SetModel($data['Model']);
             $response['OK'] = $this->Model::all();
             
-            if (!$response['OK']) {
+            if ($response['OK'] ==  null) {
                 $response['Error'] = new Exception("Error");
             }
 
@@ -33,14 +44,20 @@ class ModelRepository implements IModelRepository {
         }  
     }
 
+    /**
+     * Insert data in DB
+     *
+     * @param Array Model, Data of Models
+     * @return Array Data of DB in $response
+     */
     public function Insert($data)
     {
+        $response = array();
         try {
-            $response = [];
             $this->SetModel($data['Model']);
             $response['OK'] = $this->Model::create($data);
 
-            if (!$response['OK']) {
+            if ($response['OK'] ==  null) {
                 $response['Error'] = new Exception("Error");
             }
 
@@ -52,17 +69,25 @@ class ModelRepository implements IModelRepository {
         }
     }
 
+    /**
+     * Update data in DB
+     *
+     * @param Array Model, Data of Models and Entity
+     * @return Array Data of DB in $response
+     */
     public function Update($data)
-    {
+    { 
+        $response = array();
         try {
-            $response = [];
             $this->SetModel($data['Model']);
-            $response['OK'] = $this->Model::update($data);
-
-            if (!$response['OK']) {
+            $response['OK'] = $this->Model::find($data['Entity']['id']);
+            if ($response['OK'] ==  null) {
                 $response['Error'] = new Exception("Error");
             }
-
+            $response['OK'] = $this->Model->where('id', $data['Entity']['id'])->update($data['Entity']);
+            if ($response['OK'] ==  null) {
+                $response['Error'] = new Exception("Error");
+            }
             return $response;
 
         } catch (Exception $ex) {
@@ -71,17 +96,26 @@ class ModelRepository implements IModelRepository {
         }
     }
 
+    /**
+     * Update data in DB
+     *
+     * @param Array Model, Data of Models and Entity
+     * @return Array Data of DB in $response
+     */
     public function Delete($data)
     {
+        $response = array();
         try {
-            $response = [];
             $this->SetModel($data['Model']);
-            $response['OK'] = $this->Model::delete($data);
+            $response['OK'] = $this->Model::find($data['Entity']['id']);
 
-            if (!$response['OK']) {
+            if ($response['OK'] == null) {
                 $response['Error'] = new Exception("Error");
             }
-
+            $response['OK'] = $this->Model::delete($data);
+            if ($response['OK'] == null) {
+                $response['Error'] = new Exception("Error");
+            }
             return $response;
 
         } catch (Exception $ex) {
@@ -90,19 +124,22 @@ class ModelRepository implements IModelRepository {
         }
     }
 
+    /**
+     * Find data in DB
+     *
+     * @param Array Model, Data of Models and Entity
+     * @return Array Data of DB in $response
+     */
     public function Find($data)
     {
+        $response = array();
         try {
-            $response = [];
             $this->SetModel($data['Model']);
-            $response['OK'] = $this->Model::find($data);
-
-            if (!$response['OK']) {
+            $response['OK'] = $this->Model::find($data['Entity']['id']);
+            if ($response['OK'] == null) {
                 $response['Error'] = new Exception("Error");
             }
-
-            return $response;
-            
+            return $response;           
         } catch (Exception $ex) {
             $response['Error'] = $ex;
             return $response;
