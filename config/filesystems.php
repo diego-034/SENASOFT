d6bd1ebc2dd38925986d3aa0aea4753d562ea405
Diego <dacantero47@misena.edu.co>
2020-10-15 13:45:45 -0500

    Last Changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,14 @@ return [
             'visibility' => 'public',
         ],
 
+        'excel' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/excel'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
